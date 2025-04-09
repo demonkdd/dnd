47930d1c992d7b93cec28917faf97a53de54dbeb
C <chronocrusaderagain@yahoo.com>
2025-04-09 11:53:43 -0400

    Added pages for save data

---
+++ DnDthing2/SavedInfo.swift
@@ -0,0 +1,8 @@
+//
+//  SavedInfo.swift
+//  DnDthing2
+//
+//  Created by Cameron Slaughter on 4/9/25.
+//
+
+import Foundation
---
+++ DnDthing2/SavedInfoContainer.swift
@@ -0,0 +1,8 @@
+//
+//  SavedInfoContainer.swift
+//  DnDthing2
+//
+//  Created by Cameron Slaughter on 4/9/25.
+//
+
+import Foundation
