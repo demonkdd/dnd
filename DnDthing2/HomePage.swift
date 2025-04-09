464e4de9ea37a7d827f33175948ff84ed1749280
C <chronocrusaderagain@yahoo.com>
2025-04-09 10:03:11 -0400

    Add home page and some buttons making it possible to go through out the app

---
+++ DnDthing2/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  DnDthing2
+//
+//  Created by Cameron Slaughter on 4/9/25.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePage()
+}
